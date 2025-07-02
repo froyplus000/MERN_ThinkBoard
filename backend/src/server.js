ffa61136e4f3aefed1acf8ad957ec4bbc4929315
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-07-02 07:44:49 +0700

    MIddleware and RateLimiter Completed
    Note:
    - Rate limiter completed with Upstash (redis)
    - 100 requests per minute

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,17 +8,25 @@ import notesRoutes from "./routes/notesRoutes.js"; // use with routes prefix
 // Connect to MongoDB
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimitter from "./middleware/rateLimitter.js";
 dotenv.config();
-connectDB();
 
 // middleware
 app.use(express.json());
+app.use(rateLimitter);
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
 
 // Routes Prefix
 app.use("/api/notes", notesRoutes);
 
 const PORT = process.env.PORT || 5001;
 // NEED! : Listening on port 5001 (or other port, can specify)
-app.listen(PORT, () => {
-  console.log("SERVER IS RUNING ON PORT : ", PORT);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("SERVER IS RUNING ON PORT : ", PORT);
+  });
 });
